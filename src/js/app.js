4a52d7252af9ae3ddafe002c9b7a5e289c90a65b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-04-30 02:17:23 +0200

    feat(app): enhanced select + fetch error notifier

--- src/js/app.js
+++ src/js/app.js
@@ -15,9 +15,13 @@ const SEMVER_REGEX = () =>
     /\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b/gi;
 const DATE_REGEX = () => /(\d{4})-(\d{2})-(\d{2})/gi;
 
+const GITHUB_BOOK_SVG = '<svg class="octicon octicon-repo" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>'
+
 let headingsWithSemVer = [];
 let REPOSITORIES = [];
 let ORDERED_REPOSITORIES = [];
+let choicesItems = [];
+let choiceSelectReference;
 
 let repositoryDropdown = document.getElementById('repository-dropdown');
 
@@ -26,8 +30,95 @@ fetch(`./data/final-data.json`)
     .then(data => {
         REPOSITORIES = data;
         ORDERED_REPOSITORIES = orderRepositoriesByStars();
-        populateSelect();
+        //populateSelect();
+
+        console.log(ORDERED_REPOSITORIES);
+
+        prepareDataForChoiceSelect();
+        createChoiceSelect('stars');
     });
+
+let prepareDataForChoiceSelect = () => {
+    choicesItems = ORDERED_REPOSITORIES.map((repo) => {
+        let repoOwner = repo.repo_name.split('/')[0];
+        let repoName = repo.repo_name.split('/')[1];
+        return {
+            value: repo.repo_name,
+            label: repoOwner + ' / ' + repoName,
+            selected: false,
+            disabled: false,
+            customProperties: {
+                stars: repo.stars,
+                name: repoName,
+                owner: repoOwner
+            }
+        }
+    });
+    console.log(choiceSelectReference);
+    
+    if (choiceSelectReference) {
+        choiceSelectReference.destroy();
+    }
+}
+
+let createChoiceSelect = (filter) => {
+    let sortFilterMethod;
+    if (filter === 'stars') {
+        sortFilterMethod = function(a, b) {
+            return (a.customProperties.stars !== b.customProperties.stars ? (a.customProperties.stars < b.customProperties.stars ? 1 : -1) : 0);
+        }
+    } else {
+        sortFilterMethod = function(a, b) {
+            return (a.customProperties.name !== b.customProperties.name ? (a.customProperties.name > b.customProperties.name ? 1 : -1) : 0);
+        }
+    }
+    choiceSelectReference = new Choices(repositoryDropdown, {
+        itemSelectText: '',
+        choices: choicesItems,
+        sortFilter: sortFilterMethod,
+        callbackOnCreateTemplates: function(template) {
+            var classNames = this.config.classNames;
+            return {
+                item: data => {
+                    return template(`
+                <div class="${classNames.item} ${
+                        data.highlighted
+                            ? classNames.highlightedState
+                            : classNames.itemSelectable
+                    }" data-item data-id="${data.id}" data-value="${
+                        data.value
+                    }" ${data.active ? 'aria-selected="true"' : ''} ${
+                        data.disabled ? 'aria-disabled="true"' : ''
+                    }>
+                    ${GITHUB_BOOK_SVG}<span class="repo-name">${data.label}</span>&nbsp;<span class="list-stars list-stars-selected">${data.customProperties.stars} &#9733;</span>
+                </div>
+              `);
+                },
+                choice: data => {
+                    return template(`
+                <div class="${classNames.item} ${classNames.itemChoice} ${
+                        data.disabled
+                            ? classNames.itemDisabled
+                            : classNames.itemSelectable
+                    }" data-select-text="${
+                        this.config.itemSelectText
+                    }" data-choice ${
+                        data.disabled
+                            ? 'data-choice-disabled aria-disabled="true"'
+                            : 'data-choice-selectable'
+                    } data-id="${data.id}" data-value="${data.value}" ${
+                        data.groupId > 0
+                            ? 'role="treeitem"'
+                            : 'role="option"'
+                    }>
+                    ${GITHUB_BOOK_SVG}<span class="repo-name">${data.label}</span>&nbsp;<span class="list-stars">${data.customProperties.stars} &#9733;</span>
+                </div>
+              `);
+                }
+            };
+        }
+    });
+}
 
 let orderRepositoriesAlphabetically = () => {
     return REPOSITORIES.sort(
@@ -54,11 +145,14 @@ let populateSelect = () => {
 
     let option;
     for (let i = 0; i < ORDERED_REPOSITORIES.length; i++) {
+        let repoOwner = ORDERED_REPOSITORIES[i].repo_name.split('/')[0];
+        let repoName = ORDERED_REPOSITORIES[i].repo_name.split('/')[1];
         option = document.createElement('option');
-        option.text = `${ORDERED_REPOSITORIES[i].repo_name.split('/')[0]} - ${
-            ORDERED_REPOSITORIES[i].repo_name.split('/')[1]
-        } | ${ORDERED_REPOSITORIES[i].stars} stars`;
+        option.text = `${repoOwner} - ${repoName}`;
         option.value = ORDERED_REPOSITORIES[i].repo_name;
+        option.setAttribute('data-stars', ORDERED_REPOSITORIES[i].stars);
+        option.setAttribute('data-owner', repoOwner);
+        option.setAttribute('data-name', repoName);
         repositoryDropdown.add(option);
     }
 };
@@ -66,6 +160,13 @@ let populateSelect = () => {
 let fetchChangelog = repository => {
     function handleErrors(response) {
         if (!response.ok) {
+            notifier.show(
+                'Oups !',
+                'Could not fetch the CHANGELOG file.',
+                'danger',
+                '',
+                3000
+            );
             throw Error(response.statusText);
         }
         return response;
@@ -79,11 +180,11 @@ let fetchChangelog = repository => {
         .catch(error => console.log(error));
 };
 
-document.querySelector('select').addEventListener('change', (event) => {
+document.querySelector('select').addEventListener('change', event => {
     let repositoryName = event.currentTarget.value;
     console.log(repositoryName);
-    fetchChangelog(repositoryName)
-})
+    fetchChangelog(repositoryName);
+});
 
 /*fetch(`https://raw.githubusercontent.com/angular/angular/master/CHANGELOG.md`)
     .then(response => response.text())
@@ -190,11 +291,15 @@ let cleanOrderButtonsState = () => {
         cleanOrderButtonsState();
         event.currentTarget.classList.add('selected');
         let dataOrder = event.currentTarget.getAttribute('data-order');
+        let orderType = '';
         if (dataOrder === 'asc') {
             ORDERED_REPOSITORIES = orderRepositoriesAlphabetically();
+            orderType = 'asc';
         } else {
             ORDERED_REPOSITORIES = orderRepositoriesByStars();
+            orderType = 'stars';
         }
-        populateSelect();
+        prepareDataForChoiceSelect();
+        createChoiceSelect(orderType);
     });
 });

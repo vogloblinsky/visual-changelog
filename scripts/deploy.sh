5fa0e1c9ad2d49c1e1191759355c07c3f2945e14
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-05-02 17:20:50 +0200

    fix(deploy)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo "Starting deployment."
 echo "Target: gh-pages branch"
 
-TEMP_DIRECTORY="/tmp/__temp_static_content"
+TEMP_DIRECTORY="tmp"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
@@ -12,15 +12,9 @@ mkdir $TEMP_DIRECTORY || exit 1
 cp -r src $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
 
-echo "Checking out gh-pages branch"
-git checkout -B gh-pages || exit 1
+cd $TEMP_DIRECTORY
 
-echo "Removing old static content"
-git rm -rf . || exit 1
-
-echo "Copying newly generated static content"
-cp -r $TEMP_DIRECTORY/* . || exit 1
-cp $TEMP_DIRECTORY/.gitignore . || exit 1
+git init && \
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI" || exit 1
@@ -29,9 +23,6 @@ git config user.email "vincent.ogloblinsky@gmail.com" || exit 1
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-
-echo "Cleaning up temp files"
-rm -Rf $TEMP_DIRECTORY
 
 echo "Deployed successfully."
 exit 0

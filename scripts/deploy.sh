757d9806a0e72ca167666ea6905fbfd9f484fd18
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-05-02 16:46:48 +0200

    fix(deploy)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling content"
 mkdir $TEMP_DIRECTORY || exit 1
-cp src $TEMP_DIRECTORY || exit 1
+cp -r src $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1

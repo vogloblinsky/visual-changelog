e311867dd630e94f06d19028bd1f834f5d81cb9b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-05-04 11:47:21 +0200

    fix(app): SW js / css strategy

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{css,json,png,html,js}"
+    "**/*.{json,png,html}"
   ],
-  "swDest": "dist/sw.js"
+  "swDest": "dist/sw.js",
+  runtimeCaching: [{
+    urlPattern: /\.(?:js|css)$/,
+
+    handler: 'staleWhileRevalidate',
+
+    options: {
+      // Only cache 10 images.
+      cacheName: 'static-resources'
+    }
+  }]
 };
